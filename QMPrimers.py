4857a5d9e25f691c4e450112c413a4b4b8251512
dsoldevila <david.sundevila@gmail.com>
2019-03-07 10:59:40 +0100

    improving GUI

--- QMPrimers.py
+++ QMPrimers.py
@@ -16,13 +16,16 @@ import os
 import sys
 import _thread
 
+parameters = {"gen": "", "primer_pairs": "", "output_file": "out.csv", "mf": 5, "mr": 5, "hanging-primers": False, "matching_type(NOT IMPLEMENTED": 0}
+output_info = {"primerPair": True,"fastaid": True,"primerF": True,"primerR": True,"mismFT": True,"mismRT": True,"amplicon": True,
+               "F_pos": True, "mismFT_loc": True, "mismFT_type": True, "R_pos": True, "mismRT_loc": True, "mismRT_type": True}
 
 class GUI(Frame):
     def __init__(self, parent=Frame):
         
         self.main_frame = Frame.__init__(self, parent)
         
-        self.parameters = {"gen": None, "primer_pairs": None, "output_file": "out.csv", "mf": 5, "mr": 5, "hanging-primers": False, "matching_type(NOT IMPLEMENTED": 0}
+        self.parameters = parameters
         
         """Menu"""
         self.main_menu = Menu(parent)
@@ -31,59 +34,80 @@ class GUI(Frame):
         file.add_command(label="New")
         self.main_menu.add_cascade(label="File", menu=file)
         
+        """Frame containing Files and Other Parameters frames"""
+        self.first_row_frame = Frame(self.main_frame)
+        self.first_row_frame.pack(expand=YES, fill=BOTH)
+        
         """Select Files Frame"""
-        self.file_frame = Frame(self.main_frame)
-        self.file_frame.pack(expand=YES, fill=X)
+        self.file_frame = Frame(self.first_row_frame)
+        self.file_frame.pack(side=LEFT, expand=YES, fill=X)
+        Label(self.file_frame, text="Files").pack()
         
-        self.entry_g = Entry(self.file_frame)
-        self.entry_g.pack(side=LEFT, expand=YES, fill=X)
-        self.entry_g.insert(0, "<No Genome>")
-        self.entry_g.bind("<Return>", (lambda event: self.open_bio_files(self.entry_g.get())))
+        self.entries = {}
+        self.button = {}
+        for pkey in self.parameters:
+            if(isinstance(self.parameters[pkey], str)):
+                block = Frame(self.file_frame)
+                block.pack(expand=YES, fill=BOTH)
+                self.entries[pkey] = Entry(block)
+                self.entries[pkey].pack(side=LEFT, expand=YES, fill=X)
+                self.button[pkey] = Button(block, text="Open")
+                self.button[pkey].pack(side=LEFT, expand=NO, fill=X)
+                
+        self.entries["gen"].insert(0, "<No Genome>")
+        self.entries["gen"].bind("<Return>", (lambda event: self.open_bio_files(self.entry_g.get())))
+        self.button["gen"].config(command=(lambda: self.update_bio_files(self._open_file())))
         
-        self.button_g = Button(self.file_frame, text="Open", command=(lambda: self.update_bio_files(self._open_file())))
-        self.button_g.pack(side=LEFT, expand=NO, fill=X)
+        self.entries["primer_pairs"].insert(0, "<No Primer Pairs>")
+        self.entries["primer_pairs"].bind("<Return>", (lambda event: self.open_csv_file(self.entry_p.get())))
+        self.button["primer_pairs"].config(command=(lambda: self.update_primer_file(self._open_file())))
         
-        self.entry_p = Entry(self.file_frame)
-        self.entry_p.pack(side=LEFT, expand=YES, fill=X)
-        self.entry_p.insert(0, "<No Primer Pairs>")
-        self.entry_p.bind("<Return>", (lambda event: self.open_csv_file(self.entry_p.get())))
-        
-        self.button_p = Button(self.file_frame, text="Open", command=(lambda: self.update_primer_file(self._open_file())))
-        self.button_p.pack(side=LEFT, expand=NO, fill=X)
-        
-        self.entry_out = Entry(self.file_frame)
-        self.entry_out.pack(side=LEFT, expand=YES, fill=X)
-        self.entry_out.insert(0, "output.csv")
-        self.entry_out.bind("<Return>", (lambda event: self.set_output_file(self.entry_out.get())))
-        
-        self.button_out = Button(self.file_frame, text="Set", command=(lambda: self.set_output_file(self.entry_out.get())))
-        self.button_out.pack(side=LEFT, expand=NO, fill=X)
-        
+        self.entries["output_file"].insert(0, "output.csv")
+        self.entries["output_file"].bind("<Return>", (lambda event: self.set_output_file(self.entry_out.get())))
+        self.button["output_file"].config(text="Set", command=(lambda: self.set_output_file(self.entry_out.get())))
+            
             
         """Other parameters"""
-        self.other_frame = Frame(self.main_frame)
-        self.other_frame.pack(expand=YES, fill=X)
-        self.other_param_dict = {}
+        self.other_frame = Frame(self.first_row_frame)
+        self.other_frame.pack(side=LEFT, expand=YES, fill=X)
+        Label(self.other_frame, text="Parameters").pack()
+        self.other_param = {}
         for pkey in self.parameters:
             if(isinstance(self.parameters[pkey], bool)):
                 other = BooleanVar()
-                Checkbutton(self.other_frame, variable=other, text=pkey).pack(side=LEFT, expand=YES)
+                Checkbutton(self.other_frame, variable=other, text=pkey).pack(expand=YES)
                 other.set(self.parameters[pkey])
-                self.other_param_dict[pkey] = other
+                self.other_param[pkey] = other
             elif(isinstance(self.parameters[pkey], int)):
                 block = Frame(self.other_frame)
-                block.pack(side=LEFT, expand=YES)
+                block.pack(expand=YES)
                 Label(block, text=pkey).pack(side=LEFT)
                 other = StringVar()
                 Entry(block, textvariable=other).pack(side=LEFT, expand=YES)
                 other.set(self.parameters[pkey])
-                self.other_param_dict[pkey] = other
+                self.other_param[pkey] = other
         
+        """Output info"""
+        self.output_frame = Frame(self.main_frame)
+        self.output_frame.pack(expand=YES, fill=X)
+        Label(self.output_frame, text="Output Info").grid(row=0, column=0)
         
+        self.output_info = output_info
+        c=0
+        max_row_len=len(self.output_info)/2
+        for key in self.output_info:
+            var = BooleanVar()
+            Checkbutton(self.output_frame, variable=var, text=key).grid(row=int(1+c/max_row_len), column=int(c%max_row_len), sticky=W)
+            var.set(self.output_info[key])
+            self.output_info[key] = var
+            c = c+1
+        
+        self.buttons_frame = Frame(self.main_frame)
+        self.buttons_frame.pack(expand=YES, fill=X)
         
         """Compute"""
-        self.button_c = Button(text="Compute", command=self.compute)
-        self.button_c.pack(side=BOTTOM, expand=YES, fill=BOTH)
+        self.button_c = Button(self.buttons_frame, text="Compute", command=self.compute)
+        self.button_c.pack(expand=YES, fill=X)
         
         """Other"""
         self.current_directory = os.getcwd()
@@ -103,8 +127,8 @@ class GUI(Frame):
         Loads input genomes 
         """
         if(input_files): #is not None
-            self.entry_g.delete(0, END)
-            self.entry_g.insert(0, str(input_files))
+            self.entries["gen"].delete(0, END)
+            self.entries["gen"].insert(0, str(input_files))
             self.parameters["gen"] = input_files
         return
     
@@ -113,8 +137,8 @@ class GUI(Frame):
         Loads input primer pairs, stored in csv format
         """
         if(input_files): #is not None
-            self.entry_p.delete(0, END)
-            self.entry_p.insert(0, str(input_files))
+            self.entries["primer_pairs"].delete(0, END)
+            self.entries["primer_pairs"].insert(0, str(input_files))
             self.parameters["primer_pairs"] = input_files[0]
         return
     
@@ -126,14 +150,19 @@ class GUI(Frame):
         return
     
     def compute(self):
-        for pkey in self.other_param_dict:
-            self.parameters[pkey] = self.other_param_dict[pkey].get()
+        for pkey in self.other_param:
+            self.parameters[pkey] = self.other_param[pkey].get()
         print(self.parameters)
         self.gen_record = ld.load_bio_files(self.parameters["gen"], writable=self.parameters["hanging-primers"])
         self.primer_pairs = ld.load_csv_file(self.parameters["primer_pairs"])
         result = m.compute_gen_matching(int(self.parameters["mf"]), int(self.parameters["mr"]), self.primer_pairs, self.gen_record,
                                         hanging_primers=self.parameters["hanging-primers"])
-        header = ["primerPair","fastaid","primerF","primerR","mismFT","mismRT","amplicon", "F_pos", "mismFT_loc", "mismFT_type", "R_pos", "mismRT_loc", "mismRT_type"]
+        
+        header = []
+        for key in self.output_info:
+            if(self.output_info[key].get()):
+                header.append(key)
+                
         ld.store_matching_results(self.parameters["output_file"], result, header=header)
         print("Finished!")
         return
@@ -147,7 +176,8 @@ def get_help():
                            id;forwardPrimer;fPDNA;reversePrimer;rPDNA;ampliconMinLength;ampiconMaxLength", 
                        "--nogui": "GUI is not loaded", 
                        "--hanging-primers": "Primer pairs are allowed to match between [0-mf,len(genome)+mr] instead of just \
-                           between the length of the genome"}
+                           between the length of the genome",
+                       "-info <multiple strings>": "Select which info to output, all info by default"}
 
     print("QMPRIMERS HELP PAGE")
 
@@ -166,29 +196,39 @@ def compute_from_cl(parameters):
     return result
 
 if (__name__=="__main__"):
-    parameters = {"--help": False, "--nogui": False, "-mf": 5, "-mr": 5, "-gf": None, "-gformat": None, "-pf": None, "--hanging-primers": False}
+    only_cl_param = {"help": False, "command_line": False}
+    all_parameters = {**parameters, **only_cl_param}
+    flags = {"--help": "help", "--nogui": "command_line", "-mf": "mf", "-mr": "mr", "-gf": "gen", "-gformat": None, "-pf": "primer_pairs", "--hanging-primers": "hanging-primers"}
     i = 1
     nargs = len(sys.argv)
     
     while i < nargs:
-        if(sys.argv[i] not in parameters):
+        argv = sys.argv[i]
+        if(argv not in flags):
             print("Parameter "+str(sys.argv[i])+" unknown")
             print("Use --help to display the manual")
             exit();
-        if(sys.argv[i][:2]=="--"):
-            parameters[sys.argv[i]] = True
-        elif(sys.argv[i][0]=="-"):
-             parameters[sys.argv[i]] = sys.argv[i+1]
+            if(argv=="-info"): #Set custom output
+                for key in output_info: output_info[key] = False
+                i+=1
+                argv = sys.argv[i]
+                while(argv[0]!="-"):
+                    output_info[argv] = True
+        if(argv[:2]=="--"):
+            all_parameters[flags[sys.argv[i]]] = True
+        elif(argv[0]=="-"):
+             all_parameters[flags[sys.argv[i]]] = sys.argv[i+1]
              i+=1
         i+=1  
         
-    if(parameters["--help"]):
+    if(all_parameters[flags["--help"]]):
         get_help()
-    elif(parameters["--nogui"]):
+    elif(all_parameters[flags["--nogui"]]):
         compute_from_cl(parameters)
     else:
         root = Tk()
         root.title("QMPrimers")
+        root.geometry('800x400')
         main_window = GUI(root)
         root.mainloop()
     
